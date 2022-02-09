8faad40a8f2bed493e9fc21e70acd9eddca35b04
Anton Andersson <anton.andersson001@live.se>
2022-02-09 17:06:56 +0000

    Create Post model

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth import User
 
-# Create your models here.
+
+
+class Post(models.Model):
+
+    post_id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=300)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='forum_posts')
+    content = models.TextField(blank=True)
+    created_date = models.DateField(auto_now_add=True)
+    updated_date = models.DateField(auto_now=True)
+    likes = models.ManyToManyField(User, related_name='forum_likes')
