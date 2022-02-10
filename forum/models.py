01f576e2fd783531d79f09a357da483585ff1d56
Anton Andersson <anton.andersson001@live.se>
2022-02-10 14:01:02 +0000

    Add Category Model

--- forum/models.py
+++ forum/models.py
@@ -1,24 +1,28 @@
 from django.db import models
-from django.contrib.auth.Model import User
+from django.contrib.auth.models import User
 
+
+class Category(models.Model):
+    category = models.CharField(max_length=30, unique=True, primary_key=True)
 
 
 class Post(models.Model):
-
     post_id = models.IntegerField(primary_key=True)
     title = models.CharField(max_length=300)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='forum_posts')
     content = models.TextField(blank=True)
     created_date = models.DateField(auto_now_add=True)
     updated_date = models.DateField(auto_now=True)
-    category = models.ForeignKey(Category, related_name='post_category')
+    category = models.ForeignKey(Category, default='No Category', on_delete=models.SET_DEFAULT, related_name='post_category')
     likes = models.ManyToManyField(User, related_name='post_likes')
 
 
 class Comments(models.Model):
-
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='post_comments')
     body = models.TextField()
     created_date = models.DateField(auto_now_add=True)
     updated_date = models.DateField(auto_now=True)
     likes = models.ManyToManyField(User, related_name='comment_likes')
+
+
+
