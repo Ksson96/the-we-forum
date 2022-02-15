6735eb3e9e191857d456cfae24589f7ce6bf8150
Anton Andersson <anton.andersson001@live.se>
2022-02-15 12:48:56 +0000

    Change Nav Li options if user is logged in

--- theweforum/settings.py
+++ theweforum/settings.py
@@ -55,6 +55,10 @@ INSTALLED_APPS = [
 
 SITE_ID = 1
 
+LOGIN_REDIRECT_URL = '/'
+SIGNUP_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
